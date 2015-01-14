86c05c9d24ce5b030de01dbc89091b2e61060576
Matthew Ruttley <mruttley@mozilla.com>
2015-01-14 16:06:05 -0500

    Simple server to show the page, and pop open a window

--- runTests.js
+++ runTests.js
@@ -1,2 +1,13 @@
 //Main handler file for running tests
 
+var connect = require('connect'); 
+var serveStatic = require('serve-static'); 
+var open = require('open'); //can open up a browser window
+
+//set up basic web server
+var app = connect();
+app.use(serveStatic(__dirname + "/results_interface", {'index': ['index.html']})); //redirects for a default
+app.listen(8080);
+
+//open up the user's browser window
+open('http://localhost:8080');
