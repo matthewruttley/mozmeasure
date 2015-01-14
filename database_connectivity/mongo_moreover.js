d3bf6f8e35d787316e4e7379670423a9a718019d
Matthew Ruttley <mruttley@mozilla.com>
2015-01-14 17:00:34 -0500

    Meaningful testing message

--- database_connectivity/mongo_moreover.js
+++ database_connectivity/mongo_moreover.js
@@ -2,3 +2,10 @@
 //Moreover data is a series of pre classified urls
 
 
+function load_moreover_data(){
+	console.log('Loading moreover data...')
+}
+
+
+
+exports.load_moreover_data = load_moreover_data;
